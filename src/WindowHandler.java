8ace1eafd457383b7b4329092ad602a6d1f4167a
Adrian-92 <119793921+Adrian-92@users.noreply.github.com>
2025-01-31 17:13:18 +0100

    window handler cleanup

--- src/WindowHandler.java
+++ src/WindowHandler.java
@@ -29,16 +29,14 @@ public class WindowHandler extends JFrame {
     private static JPanel examButtonPanel;
     private static JPanel buttonPanel;
 
-
     private static TextField inputString;
+
     private static Button readFile;
     private static Button saveFile;
     private static Button search;
     private static Button sort;
     private static Button invalidStudents;
     private static Button refresh;
-
-
     private static Button exam0;
     private static Button exam1;
     private static Button exam2;
@@ -49,142 +47,10 @@ public class WindowHandler extends JFrame {
     public static void main(String[] args) {
 
         studentDataset = new StudentDataset();
-        // set window
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("PdP-Exams");
-        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        frame.setResizable(true);
-        frame.setLayout(new BorderLayout());
 
-        // set  panels and layout on side
-        buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        examButtonPanel = new JPanel();
-        examButtonPanel.setLayout(new BoxLayout(examButtonPanel, BoxLayout.X_AXIS));
-
-        // buttons and text input
-        inputString = new TextField(20);
-        readFile = new Button("read file");
-        saveFile = new Button("generate files");
-        search = new Button("search by name");
-        sort = new Button("sort by name");
-        invalidStudents = new Button("show invalid students");
-        refresh = new Button("refresh");
-
-        exam0 = new Button("0 exams");
-        exam1 = new Button("1 exam");
-        exam2 = new Button("2 exams");
-        textField = new JTextArea(OPTIONS_TEXT);
-        textField.getDocument().putProperty("filterNewlines", Boolean.FALSE);
-        textField.setEditable(false);
-        textField.setFont(new Font("Arial", Font.PLAIN, 12));
-        textField.setText("enter file path");
-
-
-        // set dimensions
+        setInterface();
+        setActionListeners();
         setButtonDimensions();
-
-
-        // add buttons to panel
-        buttonPanel.add(inputString);
-        buttonPanel.add(readFile);
-        buttonPanel.add(saveFile);
-        buttonPanel.add(search);
-        buttonPanel.add(sort);
-        buttonPanel.add(invalidStudents);
-        buttonPanel.add(refresh);
-
-        examButtonPanel.add(exam0);
-        examButtonPanel.add(exam1);
-        examButtonPanel.add(exam2);
-        buttonPanel.add(examButtonPanel);
-
-        buttonPanel.add(textField);
-
-        // set action listeners
-        readFile.addActionListener(e -> {
-                    input = inputString.getText();
-                    try {
-                        studentDataset.setDataset(Reader.readFile(input));
-                        if (studentDataset.getDataset() != null || !input.isEmpty()) {
-                            showEntries(studentDataset.getDataset());
-                            inputString.setText("");
-                            textField.setText(OPTIONS_TEXT);
-                            setButtonDimensions();
-                        }
-                    } catch (IOException | InvalidPathException ex) {
-                        System.err.println(ex.getMessage());
-                        messageDialog("enter valid filepath");
-                    }
-                }
-        );
-
-        saveFile.addActionListener(e -> {
-            input = inputString.getText();
-            if (studentDataset.getDataset() == null && !input.isEmpty()) {
-                try {
-                    studentDataset.setDataset(Reader.readFile(input));
-                } catch (IOException ex) {
-                    messageDialog("enter valid filepath");
-                }
-            }
-            if (studentDataset.getSortedStudents() != null && !studentDataset.getSortedStudents().isEmpty()) {
-                if (Writer.writeFiles(studentDataset.getSortedStudents())) {
-                    messageDialog("files generated");
-                }
-            } else if (studentDataset.getDataset() != null && !studentDataset.getDataset().isEmpty()) {
-                if (Writer.writeFiles(studentDataset.getDataset())) {
-                    messageDialog("files generated");
-                }
-            } else messageDialog("no students found");
-
-        });
-
-        search.addActionListener(e -> {
-            String searchInput = inputString.getText();
-            if (studentDataset.getDataset() != null) {
-                studentDataset.searchStudentsByName(searchInput);
-                showEntries(studentDataset.getSortedStudents());
-                inputString.setText("");
-            }
-        });
-
-        sort.addActionListener(e -> {
-            if (studentDataset.getDataset() != null) {
-                studentDataset.sortStudentsByName();
-                showEntries(studentDataset.getSortedStudents());
-            }
-        });
-        invalidStudents.addActionListener(e -> {
-            studentDataset.getInvalidStudents();
-            showEntries(studentDataset.getSortedStudents());
-        });
-
-        refresh.addActionListener(e -> {
-            studentDataset.refresh();
-            showEntries(studentDataset.getSortedStudents());
-        });
-        exam0.addActionListener(e -> {
-            studentDataset.showStudentsByExam(0);
-            showEntries(studentDataset.getSortedStudents());
-        });
-
-        exam1.addActionListener(e -> {
-            studentDataset.showStudentsByExam(1);
-            showEntries(studentDataset.getSortedStudents());
-        });
-
-        exam2.addActionListener(e -> {
-            studentDataset.showStudentsByExam(2);
-            showEntries(studentDataset.getSortedStudents());
-        });
-
-        frame.add(buttonPanel, BorderLayout.WEST);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
 
     }
 
@@ -243,6 +109,61 @@ public class WindowHandler extends JFrame {
     }
 
 
+    // add frame and buttons to window
+    public static void setInterface() {
+        // set window
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("PdP-Exams");
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        frame.setResizable(true);
+        frame.setLayout(new BorderLayout());
+
+        // set  panels and layout on side
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        examButtonPanel = new JPanel();
+        examButtonPanel.setLayout(new BoxLayout(examButtonPanel, BoxLayout.X_AXIS));
+
+        // buttons and text input
+        inputString = new TextField(20);
+        readFile = new Button("read file");
+        saveFile = new Button("generate files");
+        search = new Button("search by name");
+        sort = new Button("sort by name");
+        invalidStudents = new Button("show invalid students");
+        refresh = new Button("refresh");
+
+        exam0 = new Button("0 exams");
+        exam1 = new Button("1 exam");
+        exam2 = new Button("2 exams");
+        textField = new JTextArea(OPTIONS_TEXT);
+        textField.getDocument().putProperty("filterNewlines", Boolean.FALSE);
+        textField.setEditable(false);
+        textField.setFont(new Font("Arial", Font.PLAIN, 12));
+        textField.setText("enter file path");
+
+        // add buttons to panel
+        buttonPanel.add(inputString);
+        buttonPanel.add(readFile);
+        buttonPanel.add(saveFile);
+        buttonPanel.add(search);
+        buttonPanel.add(sort);
+        buttonPanel.add(invalidStudents);
+        buttonPanel.add(refresh);
+
+        examButtonPanel.add(exam0);
+        examButtonPanel.add(exam1);
+        examButtonPanel.add(exam2);
+        buttonPanel.add(examButtonPanel);
+        buttonPanel.add(textField);
+
+        frame.add(buttonPanel, BorderLayout.WEST);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
     public static void setButtonDimensions() {
         int textWidth = textField.getPreferredSize().width;
         int buttonWidth = BUTTON_WIDTH;
@@ -266,6 +187,87 @@ public class WindowHandler extends JFrame {
         exam0.setPreferredSize(new Dimension(smallBtnWidth, BUTTON_HEIGHT));
         exam1.setPreferredSize(new Dimension(smallBtnWidth, BUTTON_HEIGHT));
         exam2.setPreferredSize(new Dimension(smallBtnWidth, BUTTON_HEIGHT));
+    }
+
+    public static void setActionListeners() {
+        // set action listeners
+        readFile.addActionListener(e -> {
+                    input = inputString.getText();
+                    try {
+                        studentDataset.setDataset(Reader.readFile(input));
+                        if (studentDataset.getDataset() != null || !input.isEmpty()) {
+                            showEntries(studentDataset.getDataset());
+                            inputString.setText("");
+                            textField.setText(OPTIONS_TEXT);
+                            setButtonDimensions();
+                        }
+                    } catch (IOException | InvalidPathException ex) {
+                        System.err.println(ex.getMessage());
+                        messageDialog("enter valid filepath");
+                    }
+                }
+        );
+
+        saveFile.addActionListener(e -> {
+            input = inputString.getText();
+            if (studentDataset.getDataset() == null && !input.isEmpty()) {
+                try {
+                    studentDataset.setDataset(Reader.readFile(input));
+                } catch (IOException ex) {
+                    messageDialog("enter valid filepath");
+                }
+            }
+            if (studentDataset.getSortedStudents() != null && !studentDataset.getSortedStudents().isEmpty()) {
+                if (Writer.writeFiles(studentDataset.getSortedStudents())) {
+                    messageDialog("files successfully generated");
+                } else messageDialog("file generation failed");
+            } else if (studentDataset.getDataset() != null && !studentDataset.getDataset().isEmpty()) {
+                if (Writer.writeFiles(studentDataset.getDataset())) {
+                    messageDialog("files successfully generated");
+                } else messageDialog("file generation  failed");
+            } else messageDialog("no students found");
+
+        });
+
+        search.addActionListener(e -> {
+            String searchInput = inputString.getText();
+            if (studentDataset.getDataset() != null) {
+                studentDataset.searchStudentsByName(searchInput);
+                showEntries(studentDataset.getSortedStudents());
+                inputString.setText("");
+            }
+        });
+
+        sort.addActionListener(e -> {
+            if (studentDataset.getDataset() != null) {
+                studentDataset.sortStudentsByName();
+                showEntries(studentDataset.getSortedStudents());
+            }
+        });
+        invalidStudents.addActionListener(e -> {
+            studentDataset.getInvalidStudents();
+            showEntries(studentDataset.getSortedStudents());
+        });
+
+        refresh.addActionListener(e -> {
+            studentDataset.refresh();
+            showEntries(studentDataset.getSortedStudents());
+        });
+        exam0.addActionListener(e -> {
+            studentDataset.showStudentsByExam(0);
+            showEntries(studentDataset.getSortedStudents());
+        });
+
+        exam1.addActionListener(e -> {
+            studentDataset.showStudentsByExam(1);
+            showEntries(studentDataset.getSortedStudents());
+        });
+
+        exam2.addActionListener(e -> {
+            studentDataset.showStudentsByExam(2);
+            showEntries(studentDataset.getSortedStudents());
+        });
+
     }
 }
 
